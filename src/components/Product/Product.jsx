9798414f71e557ee161875d7813c6e8ffac5b7ea
Mehedi Hasan <designwithmehedi@gmail.com>
2023-12-02 09:40:36 +0600

    Product Cart Styled

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const { img, name, seller, quantity, price } = props.product
+    const { img, name, seller, ratings, price } = props.product
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h4>{name}</h4>
+            <div className='product-info'>
+                <h4 className='name'>{name}</h4>
+                <h5 className='price'>Price: ${price}</h5>
+
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {ratings}</p>
+            </div>
+            <button className='add-to-cart'>Add to Card</button>
         </div>
     );
 };
