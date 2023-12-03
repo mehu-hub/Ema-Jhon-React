947af26775414f1167029778d008935106b7b878
Mehedi Hasan <designwithmehedi@gmail.com>
2023-12-04 02:07:34 +0600

    Display local storage cart to the UI

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Cart.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight, faCartShopping, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight, faCartShopping, faTrash, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 const Cart = ({ cart }) => {
     // const cart = props.cart; --- option 1
@@ -40,7 +40,7 @@ const Cart = ({ cart }) => {
                 </div>
             </div>
             <div className='cart-btn'>
-                <button className='clear-btn'>Clear Cart <FontAwesomeIcon icon={faTrash} /></button>
+                <button className='clear-btn'>Clear Cart <FontAwesomeIcon icon={faTrashCan} /></button>
                 <button className='review-btn'>Review Order <FontAwesomeIcon icon={faArrowRight} /></button>
             </div>
         </div>
